2eba7052b27c84689bce7e0b41c31a3b7f97d25f
Alex Eftimie <alex@eftimie.ro>
2015-06-02 13:02:25 +0200

    [data] Fix elasticsearch dump state

--- data/put.sh
+++ data/put.sh
@@ -16,9 +16,12 @@ docker run --rm --volumes-from=$DATA_ID busybox chown 999:999 /var/lib/postgresq
 # Fix access
 docker run --rm --volumes-from=$DATA_ID busybox sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" /var/lib/postgresql/data/postgresql.conf
 docker run --rm --volumes-from=$DATA_ID busybox sh -c 'echo "host all all 0.0.0.0/0 trust" >> /var/lib/postgresql/data/pg_hba.conf'
+# Fix
 
 # Copy Elasticsearch files to the data container
 docker run --rm -v `pwd`/elasticsearch:/host --volumes-from=$DATA_ID busybox cp -r /host/data /usr/share/elasticsearch
+# Fix state
+docker run --rm --volumes-from=$DATA_ID busyboxrm "/usr/share/elasticsearch/data/Catalogue Cluster/nodes/0/_state/global-0.st"
 
 # Copy uploads
 docker run --rm -v `pwd`/uploads:/host --volumes-from=$DATA_ID busybox cp -r /host/document /app/public/uploads
