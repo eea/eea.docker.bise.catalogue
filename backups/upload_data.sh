c15995dc3493d4f642d364cbaf06a4059d589c40
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2016-08-11 10:13:57 +0300

    Refs #75009 BISE; improve backing up and restoring of Catalogue; added acceptance server configuration

---
+++ backups/upload_data.sh
@@ -0,0 +1,1 @@
+for DATA_ID in eeadockerbisecatalogue_data_1 eeadockerbisecatalogue_dataw1_1 eeadockerbisecatalogue_dataw2_1; do export DATA_ID; ./put.sh; done
--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
-echo "Creating containers"
+#!/bin/bash
+if [ -z $CFG ]; then
+	echo "Please set \$CFG name";
+	exit 1
+fi
+
+echo "Creating containers with $CFG"
 
 echo "Starting docker stack to create containers"
-docker-compose up -d
+docker-compose -f $CFG up -d
 
 echo "Stoping docker stack"
-docker-compose stop
+docker-compose -f $CFG stop
 
 echo "Copy backup data to containers"
-cd data
+cd backups/
 ./push_data_to_volumes.sh
 
 echo "Starting back docker stack"
 cd -
-docker-compose start
+docker-compose -f $CFG start
