861a237d9bde3918a1164073eeef8bccef7e7d14
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-10-20 13:30:40 +0300

    Improve development stack

--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -1,5 +1,10 @@
-docker exec -it eeadockerbisecatalogue_db_1 createdb -U postgres catalogue_development
-docker exec -it eeadockerbisecatalogue_db_1 createdb -U postgres catalogue_production
+echo "Warning: This will drop the existing databases!!!"
+
+docker exec -it eeadockerbisecatalogue_db_1 sh -c "dropdb -i -U postgres catalogue_development"
+docker exec -it eeadockerbisecatalogue_db_1 sh -c "dropdb -i -U postgres catalogue_production"
+
+docker exec -it eeadockerbisecatalogue_db_1 sh -c "createdb -U postgres catalogue_development"
+docker exec -it eeadockerbisecatalogue_db_1 sh -c "createdb -U postgres catalogue_production"
 
 docker exec eeadockerbisecatalogue_db_1 sh -c "/usr/bin/psql -U postgres catalogue_production < /tmp/db.sql"
 docker exec eeadockerbisecatalogue_db_1 sh -c "/usr/bin/psql -U postgres catalogue_development < /tmp/db.sql"
