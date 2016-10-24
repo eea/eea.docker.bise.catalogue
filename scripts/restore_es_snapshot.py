1b63e4b137024dd46d5e0b18b8046a5291de4417
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-10-24 14:58:31 +0300

    Added option to delete indexes

--- scripts/restore_es_snapshot.py
+++ scripts/restore_es_snapshot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import requests
 import sys
 
@@ -9,6 +10,12 @@ Snapshot: {} @ {} - {}
 \tIndexes: {}
 \tShards: {}/{}
 """
+
+
+def _get_indices(server):
+    url = 'http://{}:9200/_cluster/state'.format(server)
+    ixs = requests.get(url).json()['metadata']['indices'].keys()
+    return ixs
 
 
 def create_snapshot_location(server):
@@ -26,16 +33,15 @@ def create_snapshot_location(server):
 
 
 def _openclose_indices(server, close=True):
-    url = 'http://{}:9200/_all/_settings'.format(server)
-    all = requests.get(url).json().keys()
+    ixs = _get_indices(server)
 
     op = {
         False: '_open',
         True: '_close',
     }[close]
 
-    for ix in all:
-        print("{} index ".format(op, ix))
+    for ix in ixs:
+        print("{} index {}".format(op, ix))
         url = 'http://{}:9200/{}/{}'.format(server, ix, op)
         req = requests.post(url)
         assert req.json()['acknowledged'] is True
@@ -45,7 +51,8 @@ def make_backup(server, name):
     url = ('http://{}:9200/_snapshot/backup/{}?wait_for_completion=true'
            .format(server, name))
     res = requests.put(url).json()
-    print(res)
+    assert res.json()['accepted'] is True
+    print "Snapshot restored"
 
 
 def restore_backup(server, name):
@@ -54,6 +61,18 @@ def restore_backup(server, name):
     res = requests.post(url).json()
     _openclose_indices(server, close=False)
     print(res)
+
+
+def del_all(server):
+    _openclose_indices(server, close=False)
+    url = 'http://{}:9200/_all/_settings'.format(server)
+    all = requests.get(url).json().keys()
+
+    for ix in all:
+        print("Deleting {} index ".format(ix))
+        url = 'http://{}:9200/{}'.format(server, ix)
+        req = requests.delete(url)
+        assert req.json()['acknowledged'] is True
 
 
 def show_backups(server):
@@ -74,26 +93,34 @@ def show_backups(server):
 
 
 def main():
-    if not len(sys.argv) >= 3:
-        print("Please provide ip address and "
-              "an operation [view|backup|restore]")
-        sys.exit(1)
+    handlers = {
+        'view': lambda args: show_backups(args.hostname),
+        'backup': lambda args: make_backup(args.hostname, args.snapshot),
+        'restore': lambda args: restore_backup(args.hostname, args.snapshot),
+        'delall': lambda args: del_all(args.hostname),
+    }
 
-    cmd = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostname",
+                        help="Server hostname")
+    parser.add_argument(
+        "command",
+        help="The command you want performed. One of: " +
+             ", ".join(handlers.keys())
+    )
+    parser.add_argument("--snapshot",
+                        help="Snapshot name you want restored")
+    args = parser.parse_args()
 
-    server = sys.argv[1].strip()
+    server = args.hostname
     create_snapshot_location(server)
 
-    if cmd == "view":
-        show_backups(server)
+    def fallback(*args):
+        print "Not a command"
+        return sys.exit(1)
 
-    elif cmd == "backup":
-        name = sys.argv[3].strip()
-        make_backup(server, name)
-
-    elif cmd == "restore":
-        name = sys.argv[3].strip()
-        restore_backup(server, name)
+    cmd = args.command
+    handlers.get(cmd, fallback)(args)
 
 
 if __name__ == "__main__":
